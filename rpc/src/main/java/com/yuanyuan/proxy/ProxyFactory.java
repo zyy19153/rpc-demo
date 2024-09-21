6a29cd54faca82bcd531d2e24014503a9a623928
yuanyuan <1915349279@qq.com>
2024-09-21 16:34:03 +0800

    更新: 1. 实现了服务容错的处理 2. 实现了 mock 功能
    不足：
    1. 服务容错中的 error-callback 并没有什么特殊的处理，只是简单的返回提示信息。
    2. 其实 Provider 中对 HttpServer 的 start 调用可以继承在 rpc 中，写一个 BootStrap 类，实现启功功能，使得 Provider 的启功更加方便

--- rpc/src/main/java/com/yuanyuan/proxy/ProxyFactory.java
+++ rpc/src/main/java/com/yuanyuan/proxy/ProxyFactory.java
@@ -19,6 +19,14 @@ public class ProxyFactory {
         Object proxyInstance = Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[] {interfaceClass}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                // 假设 Provider 的服务仍未提供，可以进行 MOCK，方便 Consumer 开发
+                // System.getProperty 就是从 VM arguments 中看有没有参数，可以添加一个 -Dmock=mock:mockData 试试
+                String mock = System.getProperty("mock");
+                if (mock != null && mock.startsWith("mock:")) {
+                    return mock.replace("mock:", "");
+                }
+
                 RequestMessage message = new RequestMessage(interfaceClass.getName(),
                         method.getName(),
                         method.getParameterTypes(),
@@ -33,8 +41,25 @@ public class ProxyFactory {
                 // 这里就可以实现负载均衡的策略
                 URL url = RandomLoadBalance.random(urls);
 
-                // 服务调用
-                String res = client.send(url.getIp(), url.getPort(), message);
+                // 服务容错的处理
+                String res = null;
+                int RETRY_COUNT = 10; // 重试次数
+                while (RETRY_COUNT-- > 0) {
+                    try {
+                        // 服务调用
+                        res = client.send(url.getIp(), url.getPort(), message);
+                        break; // 成功就不用继续重试了。
+                    } catch (Exception e) {
+                        // 这里可以实现 error-callback 的机制
+                        if(RETRY_COUNT == 0){
+                            // 最后一次可以进行其他处理
+                            res = "服务出错，联系开发人员xxx@xxx.com";
+                        } else {
+                            // 前面 9 次直接重试
+                            e.printStackTrace();
+                        }
+                    }
+                }
                 return res;
             }
         });
